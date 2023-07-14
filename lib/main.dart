f271a4cb27000e00f1121b44f5ed58d9c109e817
RitaGabriel20 <127123967+Rita-Gabriel@users.noreply.github.com>
2023-07-14 21:54:28 +0300

    changeThem

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/note_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -8,6 +9,9 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NoteViews(),
+    );
   }
 }
---
+++ lib/views/note_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NoteViews extends StatelessWidget {
+  const NoteViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
